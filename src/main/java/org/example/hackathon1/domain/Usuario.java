d0a33da2d015ce6fc40b1ad9d8207e1e3944aaa9
Fatima <fatima.pacheco@utec.edu.pe>
2025-05-06 17:30:50 -0500

    mas cosas al domain

--- src/main/java/org/example/hackathon1/domain/Usuario.java
+++ src/main/java/org/example/hackathon1/domain/Usuario.java
@@ -1,4 +1,39 @@
-package org.example.hackathon1.domain;
+package com.sparky.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.hackathon1.domain.Empresa;
+
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Rol rol; // Rol del usuario (SPARKY_ADMIN, COMPANY_ADMIN, USER)
+
+    @ManyToOne
+    private Empresa empresa; // Relación con la empresa a la que pertenece el usuario
+
+    //@OneToMany(mappedBy = "usuario")
+    //private List<LimiteUsuario> limites; // Límites específicos de usuario
+
+    //@OneToMany(mappedBy = "usuario")
+    //private List<Solicitud> solicitudes; // Historial de solicitudes de IA realizadas por el usuario
 }
