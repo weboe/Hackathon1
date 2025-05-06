2cfd4c095d0cb5779540334a039622b60ac1dfe5
Fatima <fatima.pacheco@utec.edu.pe>
2025-05-06 17:33:53 -0500

    application

--- src/main/java/org/example/hackathon1/domain/Restriccion.java
+++ src/main/java/org/example/hackathon1/domain/Restriccion.java
@@ -1,5 +1,35 @@
-package org.example.hackathon1.domain;
+package com.sparky.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "restricciones")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Restriccion {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ModeloIA modelo;
+
+    @Enumerated(EnumType.STRING)
+    private TipoLimite tipoLimite;
+
+    private int valorLimite;
+    private int ventanaTiempoMinutos;
+
+    @ManyToOne
+    private Empresa empresa;
+
+    @OneToMany(mappedBy = "restriccion")
+    private List<LimiteUsuario> limitesUsuario;
 }
