768441bd5d61590d2e38e6e456d3330ae7bf10e1
Fatima <fatima.pacheco@utec.edu.pe>
2025-05-06 17:20:39 -0500

    empresa

--- src/main/java/org/example/hackathon1/domain/Empresa.java
+++ src/main/java/org/example/hackathon1/domain/Empresa.java
@@ -1,4 +1,12 @@
 package org.example.hackathon1.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Table(name = "empresa")
+@Getter
 public class Empresa {
+
 }
